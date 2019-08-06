b576734370a924dfb9973be6f30a81cbd1b75182
hs_horie.hayato <horie.hayato@heartsoft.co.jp>
2019-08-06 17:17:44 +0900

    first commit

---
+++ Xamarin_horie/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Xamarin_horie
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(true)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            //クリックイベント作成
+            NextButton.Clicked += NextButton_Clicked;
+        }
+
+        private void NextButton_Clicked(object sender, EventArgs e)
+        {
+            // 表示画面を入れ替える
+            Application.Current.MainPage = new MyPage();
+
+            // モーダル遷移
+            //Navigation.PushModalAsync(new MyPage());
+
+            // モーダレス遷移
+            //Navigation.PushAsync(new MyPage());
+        }
+    }
+}
